5002eaf660fb28496ac11cb9e7ebffb56b9f23c9
roridev <t3ctotalmenterandom1@outlook.com>
2021-03-15 01:57:03 -0300

    Adds sisbasebot initialization code

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using sisbase;
+using sisbase.Configuration;
 
 namespace siscode_helper
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+    class Program {
+
+        private static Assembly ASSEMBLY = typeof(Program).Assembly; 
+        static async Task Main(string[] args) {
+            DiscordSocketClient client = new();
+            SisbaseBot bot = new(client, new FileInfo($"{Directory.GetCurrentDirectory()}/config.json"));
+            SystemConfig systemConfig = new();
+            
+            systemConfig.Create(new FileInfo($"{Directory.GetCurrentDirectory()}/systems.json"));
+           
+            bot.UseSystemsApi(systemConfig);
+            
+            await bot.InstallCommandsAsync(ASSEMBLY);
+            await bot.InstallSystemsAsync(ASSEMBLY);
+
+            await bot.StartAsync();
         }
     }
 }
